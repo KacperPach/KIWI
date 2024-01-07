30cc8a6c414de7ac1e4921fee322b31d7b07c1be
kacper <kapsi1234@gmail.com>
2024-01-07 19:31:12 +0100

    Refactor HealthBar class to extend SimpleBar

---
+++ src/screen/components/simpleBar.ts
@@ -0,0 +1,51 @@
+import { Rect, RectComp , GameObj, Vec2} from "kaboom";
+import { START_HEALTH } from "../../constants/player_constants.js";
+import { RED, add, color, loadSprite, outline, pos, rect, sprite } from "../../game.js";
+
+export class SimpleBar {
+    hp : GameObj<RectComp>;
+    hpShadow : GameObj<RectComp>;
+    width: number;
+    MaxHP: number;
+    constructor(startPositon: Vec2, width: number, spriteName: string, spritePath: string, maxPoints: number ) {
+        this.width = width;
+        this.MaxHP = maxPoints;
+        loadSprite(spriteName, spritePath)
+        
+        add([
+            pos(this.width+10,startPositon.y-3),
+            sprite(spriteName),
+        ])
+
+        const Background = add([
+            rect(this.width,60, {radius:15}),
+            pos(startPositon),
+            color(20,20,20)
+        ])
+
+        const innerBackground = add([
+            rect(this.width-16,40, {radius:10}),
+            pos(startPositon.add(8,8)),
+            color(70,70,70)
+        ])
+
+        this.hpShadow = add([
+            rect(this.width-16,40, {radius:5}),
+            pos(startPositon.add(8,8)),
+            color(40,40,40)
+        ])
+
+        this.hp = add([
+            rect(this.width-16,40, {radius:10}),
+            pos(startPositon.add(8,8)),
+            color(192,17,17)
+        ])
+        
+    }
+
+    update(currentSataus: number) {
+        const hppx = currentSataus/this.MaxHP * this.width-16;
+        this.hp.width = hppx;
+        this.hpShadow.width = hppx+12;
+    }
+}
