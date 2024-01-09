3f05824f06b1e1f1a8f1d9c5606ef8033aef9e26
s27112-pj <39103506+KacperPach@users.noreply.github.com>
2024-01-09 23:15:24 +0100

    fix safari mobile not running at all

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ import esbuild from "esbuild"
 let ctx = await esbuild.context({
     entryPoints: ['src/main.ts'],
     bundle: true,
+    target: ['chrome58', 'safari11'],
     outdir: 'dist',
   })
   
