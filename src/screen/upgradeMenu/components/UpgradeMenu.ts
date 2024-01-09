8f5d0677fbda737b00348ad40800d89f648fc2d3
kacper <kapsi1234@gmail.com>
2024-01-09 23:38:50 +0100

    fixed sprite paths

--- src/screen/upgradeMenu/components/UpgradeMenu.ts
+++ src/screen/upgradeMenu/components/UpgradeMenu.ts
@@ -26,7 +26,7 @@ export class UpgradeMenu {
                 pos(posCenter),
                 "UpgradeMenu"]);
 
-        loadSprite("chooseText", "/src/sprites/choose.png");
+        loadSprite("chooseText", "src/sprites/choose.png");
         this.text = add([ sprite("chooseText"), z(15), scale(2), rotate(0), anchor("center"), pos(this.body.pos.add(vec2(0, -this.body.height/2 + 60)))]);
         this.text.onDraw(() => {
             this.text.rotateTo(wave(-10, 10, time()*2));
@@ -34,9 +34,9 @@ export class UpgradeMenu {
         });
         wave(-30,30, time())
 
-        this.buttons.push(new UpgradeButton(vec2(posCenter), "BarkIcon", "/src/sprites/BarkIcon.png", () => { this.destroy(); player.addItems(new Bark(player.head, player.level*0.1))}));
-        this.buttons.push(new UpgradeButton(vec2(posCenter.add(vec2(-300,0))), "BombIcon", "/src/sprites/BombIcon.png", () => {this.destroy(); player.addItems(new Bomb(player.tail, player.level*0.1))}));
-        this.buttons.push(new UpgradeButton(vec2(posCenter.add(vec2(300,0))), "TailAttackIcon", "/src/sprites/TailAttackIcon.png",() => {this.destroy(); player.addItems(new TailAttack(player.tail, player.level*0.1))}));
+        this.buttons.push(new UpgradeButton(vec2(posCenter), "BarkIcon", "src/sprites/BarkIcon.png", () => { this.destroy(); player.addItems(new Bark(player.head, player.level*0.1))}));
+        this.buttons.push(new UpgradeButton(vec2(posCenter.add(vec2(-300,0))), "BombIcon", "src/sprites/BombIcon.png", () => {this.destroy(); player.addItems(new Bomb(player.tail, player.level*0.1))}));
+        this.buttons.push(new UpgradeButton(vec2(posCenter.add(vec2(300,0))), "TailAttackIcon", "src/sprites/TailAttackIcon.png",() => {this.destroy(); player.addItems(new TailAttack(player.tail, player.level*0.1))}));
 
     }
     
