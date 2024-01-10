04195eb30054b8ecaa244d949ea868d0d373bb38
kacper <kapsi1234@gmail.com>
2024-01-10 20:21:59 +0100

    Add new camera functions and check for mobile device

--- src/game.ts
+++ src/game.ts
@@ -2,4 +2,4 @@ import kaboom from 'kaboom';
 const k = kaboom({});
 export const {width,rgb,drawPolygon,deg2rad,height,area, add, pos,mousePos, polygon, circle, vec2, color,opacity,loadShaderURL, uvquad, usePostEffect, rotate,rect, loadShader,anchor, follow, outline, shader} = k
 export const {RED, BLACK, GREEN, LEFT, RIGHT, UP, DOWN} = k;
-export const {health,onMouseDown, debug,tween, loop, easings, wave, text, z, onKeyDown, drawSprite, rad2deg ,onUpdate, drawLine, drawCircle, drawRect, onDraw ,wait, loadSprite, sprite,time, scale, onCollide, addKaboom, dt} = k
+export const {health,onMouseDown, camScale, camPos, debug,tween, loop, easings, wave, text, z, onKeyDown, drawSprite, rad2deg ,onUpdate, drawLine, drawCircle, drawRect, onDraw ,wait, loadSprite, sprite,time, scale, onCollide, addKaboom, dt} = k
